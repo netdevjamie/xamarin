3479407af121eef1a925a1f4559e5aa3e4338b99
Jamie <you@example.com>
2018-12-18 12:21:38 -0500

    init commit

---
+++ TipCalc.Forms.Droid/obj/Debug/android/src/md5189581140f6a380024ad6454490df57f/MainActivity.java
@@ -0,0 +1,47 @@
+package md5189581140f6a380024ad6454490df57f;
+
+
+public class MainActivity
+	extends md5780ac3a518e377dddea8f08a7fc0d1bd.MvxFormsAppCompatActivity_3
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TipCalc.Forms.Droid.MainActivity, TipCalc.Forms.Droid", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("TipCalc.Forms.Droid.MainActivity, TipCalc.Forms.Droid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
